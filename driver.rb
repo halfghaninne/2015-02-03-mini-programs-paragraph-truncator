0a8af587b0c2aa227aaaea52f4a92aa2930a4036
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-03 14:44:39 -0600

    Save of ParagraphTruncator class and Driver class. Need to debug, test, and add documentation.

---
+++ driver.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require_relative 'paragraph-truncator'
+
+class Driver
+  
+  def initialize
+    
+    puts "Please enter the paragraph you would like to truncate:"
+    para = gets.chomp.to_s
+    
+    puts "How many characters would you like to remain visible?"
+    characters_visible = gets.chomp.to_i
+    
+    puts "How would you like the paragraph to be truncated? (ex: '...' or 'Read More' or 'Click through')"
+    trunc_phrase = gets.chomp.to_s
+    
+    return_para = ParagraphTruncator.new(para, characters_visible, trunc_phrase)
+    return_para.cut_paragraph
+    puts return_para.final_truncation
+    
+    # the puts method above puts the correct output, letting me know that my object method final_truncation is working correctly
+    #
+    # I tried to display that result in a more formatted way where the final_truncation had a placeholder variable and it was put to the screen with human language:
+    #
+    # a = return_para.final_truncation
+    #
+    # puts "Here is your truncated paragraph: #{a}"
+    #
+    # But I'm getting an empty space where that variable should be.
+
+  end
+  
+end
---
+++ paragraph-truncator.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class ParagraphTruncator
+  
+  #re-write with Options Hash later
+  # => options is a Hash, with the following keys
+  # para
+  # characters_visible
+  # trunc_phrase
+  def initialize(para, characters_visible, trunc_phrase)
+    @para = para.to_s
+    @characters = characters_visible.to_i
+    @phrase = trunc_phrase.to_s
+
+  end
+  
+  def cut_paragraph
+    @short_para = @para.byteslice(0, @characters)
+  end
+  
+  def final_truncation
+    puts "#{@short_para} #{@phrase}"
+  end
+
+end
